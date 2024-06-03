4078b1c8698818605cfaf2a5d79663366e31604b
yulia-bel <yuliya.beliakova@gmail.com>
2024-06-03 16:26:12 +0200

    Add strapi documentation plugin

--- cms/config/plugins.ts
+++ cms/config/plugins.ts
@@ -1,5 +1,94 @@
 module.exports = ({ env }) => ({
   "config-sync": {
     enabled: true,
-  }
+  },
+  documentation: {
+    config: {
+      "x-strapi-config": {
+        mutateDocumentation: (generatedDocumentationDraft) => {
+          Object.keys(generatedDocumentationDraft.paths).forEach((path) => {
+            // mutate `fields` to string array
+            if (generatedDocumentationDraft.paths[path].get?.parameters) {
+              const fields = generatedDocumentationDraft.paths[path].get.parameters.find((param) => param.name === "fields");
+
+              if (fields) {
+                const fieldsIndex = generatedDocumentationDraft.paths[path].get.parameters.findIndex((param) => param.name === "fields");
+                generatedDocumentationDraft.paths[path].get.parameters[fieldsIndex] = {
+                  "name": "fields",
+                  "in": "query",
+                  "description": "Fields to return (ex: ['title','author','test'])",
+                  "deprecated": false,
+                  "required": false,
+                  "schema": {
+                    "type": "array",
+                    "items": {
+                      "type": "string"
+                    }
+                  }
+                };
+              }
+            }
+
+            // mutate `populate` to one of string | object
+            if (generatedDocumentationDraft.paths[path].get?.parameters) {
+              const populate = generatedDocumentationDraft.paths[path].get.parameters.find((param) => param.name === "populate");
+
+              if (populate) {
+                const populateIndex = generatedDocumentationDraft.paths[path].get.parameters.findIndex((param) => param.name === "populate");
+                generatedDocumentationDraft.paths[path].get.parameters[populateIndex] = {
+                  "name": "populate",
+                  "in": "query",
+                  "description": "Relations to return",
+                  "deprecated": false,
+                  "required": false,
+                  "schema": {
+                    "oneOf": [
+                      {
+                        "type": "string"
+                      },
+                      {
+                        "type": "object",
+
+                      }
+                    ]
+                  }
+                };
+              }
+            }
+
+            // check if it has {id} in the path
+            if (path.includes("{id}")) {
+              // add `populate` as params
+              if (generatedDocumentationDraft.paths[path].get) {
+                const populate = generatedDocumentationDraft.paths[path].get.parameters.find((param) => param.name === "populate");
+
+                if (!populate) {
+                  generatedDocumentationDraft.paths[path].get.parameters.push(
+                    {
+                      "name": "populate",
+                      "in": "query",
+                      "description": "Relations to return",
+                      "deprecated": false,
+                      "required": false,
+                      "schema": {
+                        "oneOf": [
+                          {
+                            "type": "string"
+                          },
+                          {
+                            "type": "object",
+
+                          }
+                        ]
+                      }
+                    },
+                  );
+                }
+              }
+            }
+          });
+        },
+      },
+    },
+  },
 });
