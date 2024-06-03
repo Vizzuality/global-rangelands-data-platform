914a917c15239c0103dd9c2f61a7f3fdbc75f2b7
barbara-chaves <barbarabarcellos90@gmail.com>
2024-06-03 10:22:55 +0200

    Add deckgl
    Add layer-manager
    
    Add missing libraries
    
    Change json-converter config from 'require' to 'imported'
    
    Add get color config function
    
    Replace next-usequerystate for nuqs

---
+++ client/src/app/[locale]/layout-providers.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { PropsWithChildren, useState } from "react";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+export default function LayoutProviders({ children }: PropsWithChildren) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </>
+  );
+}
---
+++ client/src/types/layers.ts
@@ -0,0 +1,54 @@
+import { AnyLayer, AnySource } from "react-map-gl";
+
+import { FormatProps } from "@/lib/json-converter/utils/formats";
+
+// import type { Layer } from "@/types/generated/strapi.schemas";
+
+export type Config = {
+  source?: AnySource;
+  styles?: AnyLayer[];
+};
+
+export type ParamsConfigValue = {
+  key: string;
+  default: unknown;
+};
+
+export type ParamsConfig = ParamsConfigValue[];
+
+export type LegendConfig = {
+  type: "basic" | "gradient" | "choropleth";
+  items: {
+    value: string;
+    color: string;
+  }[];
+};
+
+export type InteractionConfig = {
+  enabled: boolean;
+  events: {
+    type: "click" | "hover";
+    values: {
+      key: string;
+      label: string;
+      format?: FormatProps;
+    }[];
+  }[];
+};
+
+export type LayerProps = {
+  id?: string;
+  zIndex?: number;
+  onAdd?: (props: Config) => void;
+  onRemove?: (props: Config) => void;
+};
+
+export type LayerTyped = {
+  id: number;
+  type: string;
+  config: Config;
+  params_config: ParamsConfig;
+  legend_config: LegendConfig;
+  interaction_config: InteractionConfig;
+  metadata: Record<string, unknown>;
+};
