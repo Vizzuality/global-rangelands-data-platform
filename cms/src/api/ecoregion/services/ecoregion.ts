57f5213b999f7e55bd11dfdd571577dbb18c4ad8
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2024-06-06 12:39:11 +0200

    Slug system: dataset, layer, rangeland, ecoregion

---
+++ cms/src/api/ecoregion/services/ecoregion.ts
@@ -0,0 +1,7 @@
+/**
+ * ecoregion service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::ecoregion.ecoregion');
---
+++ cms/src/api/rangeland/controllers/rangeland.ts
@@ -0,0 +1,7 @@
+/**
+ * rangeland controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::rangeland.rangeland');
