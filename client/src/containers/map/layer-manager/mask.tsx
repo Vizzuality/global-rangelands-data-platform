85def168e5901650cee35eb6f6714206bbb321c1
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2024-06-12 15:00:17 +0200

    Masks: masking for every layer

--- client/src/containers/map/layer-manager/mask.tsx
+++ client/src/containers/map/layer-manager/mask.tsx
@@ -5,7 +5,7 @@ import { DataFilterExtension, DataFilterExtensionProps } from "@deck.gl/extensio
 
 import DeckLayer from "@/components/map/layers/deck-layer";
 import { useMemo } from "react";
-import { useSyncRangelandRegions } from "@/store/map";
+import { useSyncRangelandRegions, useSyncRangelandType } from "@/store/map";
 import { useGetRangelands } from "@/types/generated/rangeland";
 
 interface MaskProps {
@@ -15,27 +15,62 @@ interface MaskProps {
 }
 
 const Mask = ({ id, beforeId }: MaskProps) => {
+  const [rangelandType] = useSyncRangelandType();
   const [rangelandRegions] = useSyncRangelandRegions();
   const { data: rangelandsData } = useGetRangelands({
     populate: "*",
     sort: "title:asc",
   });
 
-  console.log(rangelandsData);
-
   const biomes = useMemo(() => {
-    return rangelandsData?.data
-      ?.filter((r) => {
-        if (rangelandRegions.length === 0) return true;
-        return rangelandRegions.includes(r.attributes?.code || "");
-      })
-      ?.map((r) => +(r.attributes?.code || 0));
+    return (
+      rangelandsData?.data
+        ?.map((r) => +(r.attributes?.code || 0) ?? [])
+        ?.filter((r) => {
+          if (rangelandRegions.length === 0) return true;
+          return rangelandRegions.includes(`${r}`);
+        }) ?? []
+    );
   }, [rangelandRegions, rangelandsData]);
+
+  const ecoregions = useMemo(() => {
+    return (
+      rangelandsData?.data
+        ?.map((r) => r.attributes?.ecoregions?.data?.map((e) => +(e.attributes?.code || 0)) ?? [])
+        ?.flat()
+        ?.filter((r) => {
+          if (rangelandRegions.length === 0) return true;
+          return rangelandRegions.includes(`${r}`);
+        }) ?? []
+    );
+  }, [rangelandRegions, rangelandsData]);
+
+  const filterCategories = useMemo(() => {
+    if (rangelandType === "rangeland-biomes") return biomes;
+    if (rangelandType === "rangeland-ecoregions") return ecoregions;
+    return [-1];
+  }, [rangelandType, biomes, ecoregions]);
 
   const c = useMemo(() => {
     return new GeoJsonLayer<
       {
+        objectid: number;
+        eco_name: string;
         biome_num: number;
+        biome_name: string;
+        realm: string;
+        eco_biome_: string;
+        nnh: number;
+        nnh_name: string;
+        eco_id: number;
+        shape_leng: number;
+        shape_area: number;
+        color: string;
+        color_bio: string;
+        color_nnh: string;
+        license: string;
+        area_km2: number;
+        percentage: number;
       },
       DataFilterExtensionProps
     >({
@@ -47,11 +82,20 @@ const Mask = ({ id, beforeId }: MaskProps) => {
       visible: true,
       opacity: 1,
       pickable: false,
-      getFilterCategory: (f) => f.properties.biome_num,
-      filterCategories: biomes,
+      getFilterCategory: (f) => {
+        if (rangelandType === "rangeland-ecoregions") return f.properties.eco_id;
+        if (rangelandType === "rangeland-biomes") return f.properties.biome_num;
+
+        return -1;
+      },
+      filterCategories,
       extensions: [new DataFilterExtension({ categorySize: 1 })],
+      updateTriggers: {
+        getFilterCategory: rangelandType,
+        filterCategories: filterCategories,
+      },
     });
-  }, [id, beforeId, biomes]);
+  }, [id, beforeId, rangelandType, filterCategories]);
 
   return (
     <>
