58e678da6595859c7f2a475976777721fbf0c998
Kevin <kew.sanchez@gmail.com>
2024-06-19 10:56:43 +0200

    chore(CloudFunction):  Improves logs and image download error handling

--- cloud_functions/earth_engine_tiler/src/index.ts
+++ cloud_functions/earth_engine_tiler/src/index.ts
@@ -8,6 +8,7 @@ import {EarthEngineDataset} from "./geeAssets/earth-engine-dataset";
 import {TileRequestDTO, Tilesets} from "./tile-request.dto";
 import {default as fetch , Response as FetchResponse} from "node-fetch";
 import {pipeline} from "stream/promises";
+import * as crypto from "crypto";
 
 //Asset Mapping
 const assets: Record<Tilesets, EarthEngineDataset> = {
@@ -22,6 +23,8 @@ exports.eetApp = app;
 
 
 router.get('/:z/:x/:y', async (req: Request, res: Response) : Promise<void> => {
+  const logId = crypto.createHash('sha1').update(performance.now().toString()).digest('hex');
+
   ///// This block handles CORS
   res.set('Access-Control-Allow-Origin', '*');
   if (req.method === 'OPTIONS') {
@@ -48,19 +51,18 @@ router.get('/:z/:x/:y', async (req: Request, res: Response) : Promise<void> => {
 
     asset.isYearValid(tileRequestDTO.year); //Year might be required or not depending on the asset
   } catch (errors) {
-    sendErrorResponse(res, 400, errors)
+    sendErrorResponse(res, logId, 400, errors)
     return;
   }
 
   const { tileset, x, y, z, year } = tileRequestDTO;
-  console.log(`Requesting tile for ${tileset} with coordinates ${x}-${y}-${z} and year ${year || 'N/A'}`)
+  console.log(`${logId} - Requesting tile for ${tileset} with coordinates ${x}-${y}-${z} and year ${year || 'N/A'}`)
 
   try {
     await EarthEngineUtils.authenticate();
 
     const tileURL = await asset.getMapUrl(z, x, y, year);
-    console.log(`Obtained tile URL on ${tileURL}`)
-
+    console.log(`${logId} - Obtained tile URL on ${tileURL}`)
     //TODO CACHING
     // The calculations when requesting the tile URL take the longest, images are not probably going to be very big (not even MBs)
     // create new schema on Strapi DB instance, can't use same schema without workarounds, redis on memorystore might be overkill
@@ -71,13 +73,14 @@ router.get('/:z/:x/:y', async (req: Request, res: Response) : Promise<void> => {
     const contentType = imageResponse.headers.get('content-type');
 
     if(!imageResponse.ok || !contentType || !imageResponse.body){
-      throw new Error (`A problem ocurred retrieving the tile on ${tileURL}`)
+      const errorResponse = imageResponse.body ? JSON.stringify(imageResponse.body) : 'N/A';
+      throw new Error (`A problem ocurred retrieving the tile on ${tileURL}. Status: ${imageResponse.status} - Error Response: ${errorResponse}`)
     }
 
     res.status(200).contentType(contentType);
     await pipeline(imageResponse.body, res);
   } catch (error) {
-    sendErrorResponse(res, 500, error);
+    sendErrorResponse(res, logId, 500, error);
   }
 });
 
@@ -96,7 +99,7 @@ async function getAndValidateRequestDTO(req: Request): Promise<TileRequestDTO> {
   return result;
 }
 
-function sendErrorResponse(res: Response, status: number, errors: any){
+function sendErrorResponse(res: Response, logId: string, status: number, errors: any){
   // Using class validator's validateOrReject, rejects by throwing a list of ValidationErrors, but native Errors are thrown
   // as a single non-array Error object, so this check must be done first to process the errors as a list later
   errors = errors.length ? errors : [errors];
@@ -109,7 +112,7 @@ function sendErrorResponse(res: Response, status: number, errors: any){
     error instanceof ValidationError ?
       error : { message: error.message }
   )
-  console.log('Returning Errors: ' + JSON.stringify(responseErrors))
+  console.log(`${logId} - Returning Errors:  ${JSON.stringify(responseErrors)}`)
 
   res.status( status ).json( { errors: responseErrors } )
 }
