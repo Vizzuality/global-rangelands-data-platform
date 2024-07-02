7d141336d25a5e0df892fdc4f5d16338e432f644
barbara-chaves <barbarabarcellos90@gmail.com>
2024-07-02 17:07:44 +0200

    Add dataset prefetch to map page

--- client/src/app/[locale]/map/page.tsx
+++ client/src/app/[locale]/map/page.tsx
@@ -1,15 +1,40 @@
 import Datasets from "@/containers/datasets";
 import Map from "@/containers/map";
 import Sidebar from "@/containers/sidebar";
+import getQueryClient from "@/lib/react-query/getQueryClient";
+import { getGetDatasetsQueryOptions } from "@/types/generated/dataset";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
+async function prefetchQueries() {
+  const queryClient = getQueryClient();
+  try {
+    const { queryKey, queryFn } = getGetDatasetsQueryOptions({
+      populate: ["layers", "layers.layer", "sources", "citations"],
+      sort: "id:asc",
+      locale: "all",
+    });
+
+    await queryClient.prefetchQuery({
+      queryKey,
+      queryFn,
+    });
+    return dehydrate(queryClient);
+  } catch (error) {
+    console.info(error);
+    return null;
+  }
+}
 
 export default async function Home() {
+  const dehydratedState = await prefetchQueries();
   return (
-    // The map is a client component, so it needs to be wrapped in the NextIntlClientProvider to provide the translations
-    <div className="flex h-[var(--content-height)] w-full overflow-y-hidden">
-      <Sidebar>
-        <Datasets />
-      </Sidebar>
-      <Map />
-    </div>
+    <HydrationBoundary state={dehydratedState}>
+      <div className="flex h-[var(--content-height)] w-full overflow-y-hidden">
+        <Sidebar>
+          <Datasets />
+        </Sidebar>
+        <Map />
+      </div>
+    </HydrationBoundary>
   );
 }
