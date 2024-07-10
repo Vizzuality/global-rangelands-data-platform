c127c71c402b3713735ae4077be939a7952b113e
barbara-chaves <barbarabarcellos90@gmail.com>
2024-07-10 11:26:52 +0200

    Fix header title width

--- client/src/components/ui/home-link.tsx
+++ client/src/components/ui/home-link.tsx
@@ -13,8 +13,10 @@ const HomeLink = ({ className }: HomeLinkProps) => {
   const t = useTranslations();
   return (
     <Link href="/" className="flex gap-4">
-      <RangelandLogoIcon className="fill-global" />
-      <h1 className={cn("w-32 text-sm", className)}>{t("Rangelands Data Platform")}</h1>
+      <RangelandLogoIcon className="shrink-0 fill-global" />
+      <h1 className={cn("w-36 text-balance text-sm", className)}>
+        {t("Rangelands Data Platform")}
+      </h1>
     </Link>
   );
 };
