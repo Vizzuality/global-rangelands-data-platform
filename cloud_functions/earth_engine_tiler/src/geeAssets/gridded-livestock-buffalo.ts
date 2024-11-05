05f765a3512dfe78ee4e1a1be01ec94f505d8cb9
sofia <sofia.aldabet@gmail.com>
2024-11-05 12:38:36 +0100

    addition of gridded livestock layers

---
+++ cloud_functions/earth_engine_tiler/src/geeAssets/gridded-livestock-buffalo.ts
@@ -0,0 +1,30 @@
+import { CategoricalDataset } from './earth-engine-dataset';
+import ee from '@google/earthengine';
+import { EarthEngineUtils } from "../earth-engine-utils";
+import { GriddedLivestockCommonStyles} from "./common-styles-utils";
+
+export const GriddedLivestockBuffalo: CategoricalDataset = {
+    assetPath: {
+        default: "projects/gmvad-grass/assets/5_Bf_2015_Da"
+    },
+
+    bandName: 'b1',
+
+    sldStyles: GriddedLivestockCommonStyles,
+    
+
+    areYearsValid(startYear?: number, endYear?: number): boolean {
+        // This Asset is static, and year selector is irrelevant
+        return true;
+    },
+
+    getEEAsset() {
+        return ee.Image(this.assetPath.default);
+    },
+
+    async getMapUrl(z, x, y) {
+        const image = this.getEEAsset().select(this.bandName).sldStyle(this.sldStyles);
+        const mapId = await EarthEngineUtils.getMapId(image);
+        return ee.data.getTileUrl(mapId, x, y, z);
+    },
+};
