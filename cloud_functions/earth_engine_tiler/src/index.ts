21883669586f006f981e0acd4efafecf2ccf4994
sofia <sofia.aldabet@gmail.com>
2024-11-05 12:38:36 +0100

    addition of gridded livestock layers

--- cloud_functions/earth_engine_tiler/src/index.ts
+++ cloud_functions/earth_engine_tiler/src/index.ts
@@ -8,6 +8,11 @@ import {ModisNetPrimaryProductionChange} from './geeAssets/modis-net-primary-pro
 import {AnthropogenicBiomes} from './geeAssets/anthropogenic-biomes';
 import {LivestockProductionSystems} from './geeAssets/livestock-production-systems';
 import {ForestLoss} from './geeAssets/forest-loss';
+import {GriddedLivestockCattle} from './geeAssets/gridded-livestock-cattle';
+import {GriddedLivestockGoat} from './geeAssets/gridded-livestock-goat';
+import {GriddedLivestockHorse} from './geeAssets/gridded-livestock-horse'; 
+import {GriddedLivestockBuffalo} from './geeAssets/gridded-livestock-buffalo'; 
+import {GriddedLivestockSheep} from './geeAssets/gridded-livestock-sheep'; 
 import {EarthEngineDataset} from "./geeAssets/earth-engine-dataset";
 import {TileRequestDTO, Tilesets} from "./tile-request.dto";
 import {default as fetch , Response as FetchResponse} from "node-fetch";
@@ -20,7 +25,12 @@ const assets: Record<Tilesets, EarthEngineDataset> = {
   [Tilesets.modis_net_primary_production_change]: ModisNetPrimaryProductionChange,
   [Tilesets.anthropogenic_biomes]: AnthropogenicBiomes,
   [Tilesets.livestock_production_systems]: LivestockProductionSystems,
-  [Tilesets.forest_loss]: ForestLoss
+  [Tilesets.forest_loss]: ForestLoss,
+  [Tilesets.gridded_livestock_cattle]: GriddedLivestockCattle,
+  [Tilesets.gridded_livestock_goat]: GriddedLivestockGoat,
+  [Tilesets.gridded_livestock_horse]: GriddedLivestockHorse,
+  [Tilesets.gridded_livestock_buffalo]: GriddedLivestockBuffalo,
+  [Tilesets.gridded_livestock_sheep]: GriddedLivestockSheep,
 }
 
 //We're using express to simplify path parameter parsing for the Tiles endpoint
--- cloud_functions/earth_engine_tiler/src/tile-request.dto.ts
+++ cloud_functions/earth_engine_tiler/src/tile-request.dto.ts
@@ -6,7 +6,12 @@ export enum Tilesets {
   modis_net_primary_production_change = "modis_net_primary_production_change",
   anthropogenic_biomes = "anthropogenic_biomes",
   livestock_production_systems = "livestock_production_systems",
-  forest_loss = "forest_loss"
+  forest_loss = "forest_loss",
+  gridded_livestock_cattle = "gridded_livestock_cattle",
+  gridded_livestock_goat = "gridded_livestock_goat",
+  gridded_livestock_horse = "gridded_livestock_horse",
+  gridded_livestock_buffalo = "gridded_livestock_buffalo",
+  gridded_livestock_sheep = "gridded_livestock_sheep"
 }
 
 
