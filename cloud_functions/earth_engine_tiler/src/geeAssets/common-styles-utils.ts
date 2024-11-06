61b4a870f4ab137c38e05f51c5e92adb0c677509
sofia <sofia.aldabet@gmail.com>
2024-11-06 16:22:45 +0100

    change gridded livestock color bins

--- cloud_functions/earth_engine_tiler/src/geeAssets/common-styles-utils.ts
+++ cloud_functions/earth_engine_tiler/src/geeAssets/common-styles-utils.ts
@@ -2,16 +2,13 @@ export const GriddedLivestockCommonStyles =
 
 '<RasterSymbolizer>' +
     '<ColorMap type="ramp" extended="false">' +
-        '<ColorMapEntry color="#FFFFFF" quantity="0" label="0" />' +  
-        '<ColorMapEntry color="#E0F7FA" quantity="5" label="≤ 5" />' + 
-        '<ColorMapEntry color="#B2EBF2" quantity="10" label="≤ 10" />' + 
-        '<ColorMapEntry color="#80DEEA" quantity="20" label="≤ 20" />' + 
-        '<ColorMapEntry color="#68B0C3" quantity="50" label="≤ 50" />' + 
-        '<ColorMapEntry color="#4A99B4" quantity="100" label="≤ 100" />' + 
-        '<ColorMapEntry color="#3F8B9C" quantity="250" label="≤ 250" />' + 
-        '<ColorMapEntry color="#c766fc" quantity="500" label="≤ 500" />' + 
-        '<ColorMapEntry color="#9c50c7" quantity="1000" label="≤ 1,000" />' + 
-        '<ColorMapEntry color="#883886" quantity="2500" label="≤ 2,500" />' + 
-        '<ColorMapEntry color="#390147" quantity="300000" label="> 2,500" />' + 
+        '<ColorMapEntry color="#ffffff" quantity="0" label="0" />' +  
+        '<ColorMapEntry color="#b2ebf2" quantity="50" label="≤ 50" />' + 
+        '<ColorMapEntry color="#80deea" quantity="100" label="≤ 100" />' + 
+        '<ColorMapEntry color="#68b0c3" quantity="250" label="≤ 250" />' + 
+        '<ColorMapEntry color="#3f8b9c" quantity="500" label="≤ 500" />' + 
+        '<ColorMapEntry color="#c766fc" quantity="1000" label="≤ 1,000" />' + 
+        '<ColorMapEntry color="#883886" quantity="5000" label="≤ 5,000" />' + 
+        '<ColorMapEntry color="#390147" quantity="300000" label="> 5,000" />' + 
     '</ColorMap>' +
 '</RasterSymbolizer>'
