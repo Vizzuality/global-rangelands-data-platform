19f669457bd7f5f95be01a59bcab43960d08846c
sofia <sofia.aldabet@gmail.com>
2025-03-06 14:02:12 +0100

    changing vizParams in tree cover loss

--- cloud_functions/earth_engine_tiler/src/geeAssets/forest-loss.ts
+++ cloud_functions/earth_engine_tiler/src/geeAssets/forest-loss.ts
@@ -12,7 +12,32 @@ export const ForestLoss: ContinuousDataset = {
     bands: ['lossyear'],
     min: 0,
     max: 23,
-    palette: ['#F1EEF6', '#C37D9F', '#760B39']
+    palette: [
+      '#F1EEF6', // 2000
+      '#ECE2ED', // 2001
+      '#E7D7E4', // 2002
+      '#E3CCDB', // 2003
+      '#DEC0D3', // 2004
+      '#DAB5CA', // 2005
+      '#D5AAC1', // 2006
+      '#D09EB9', // 2007
+      '#CC93B0', // 2008
+      '#C788A7', // 2009
+      '#C37D9F', // 2010
+      '#BB7194', // 2011
+      '#B3668A', // 2012
+      '#AB5A80', // 2013
+      '#A44F76', // 2014
+      '#9C446C', // 2015
+      '#943861', // 2016
+      '#8D2D57', // 2017
+      '#85214D', // 2018
+      '#7D1643', // 2019
+      '#760B39', // 2020
+      '#760B39', // 2021
+      '#760B39', // 2022
+      '#760B39'  // 2023
+    ]
   },
 
   areYearsValid (startYear?: number, endYear?: number) : boolean {
