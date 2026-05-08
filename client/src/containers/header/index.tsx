c8f8f80057046e0228b36ec27c9a922f79f26c04
mitoperni.vizzuality <miguel.toyas@vizzuality.com>
2026-05-08 10:13:22 +0200

    chore(header): remove dead language-selector duplicate

--- client/src/containers/header/index.tsx
+++ client/src/containers/header/index.tsx
@@ -1,5 +1,4 @@
 import { cn } from "@/lib/utils";
-// import LanguageSelector from "./language-selector";
 import HeaderNavigation from "./navigation";
 import LanguageSelector from "../language-selector";
 
